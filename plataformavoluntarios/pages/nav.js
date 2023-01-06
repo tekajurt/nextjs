4dfb70dae4ec8d32d004c61e1649572611450573
tekajurt <danielalejandro.hurtado@alumnos.ulagos.cl>
2023-01-06 23:52:15 +0100

    playgin with shit

--- plataformavoluntarios/pages/nav.js
+++ plataformavoluntarios/pages/nav.js
@@ -2,7 +2,7 @@
 
 const nav = () => {
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <nav className="navbar navbar-expand-lg navbar-light text-dark bg-light">
             <a className="navbar-brand" href="#">Navbar</a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
